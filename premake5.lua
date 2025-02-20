ee02e64596028ce0da33e12d9c0cab0167508c00
Vinicius Miguel <vmiguel1@gmail.com>
2025-02-20 22:17:44 +0100

    updating premake and project name

--- premake5.lua
+++ premake5.lua
@@ -2,24 +2,24 @@ OblikovatiRootDirectory = os.getenv("OBLIKOVATI_DIR")
 
 include (path.join(OblikovatiRootDirectory, "lib_vendor_source", "Coral", "Coral.Managed"))
 
-project "Oblikovati-API"
+project "Oblikovati.API"
 	kind "SharedLib"
 	language "C#"
 	dotnetframework "net9.0"
 	clr "Unsafe"
-	targetdir "%{OblikovatiRootDirectory}/oblikovati_runtime/Resources/Scripts"
-	objdir "%{OblikovatiRootDirectory}/oblikovati_runtime/Resources/Scripts/Intermediates"
+	targetdir "%{OblikovatiRootDirectory}/Oblikovati/Resources/Scripts"
+	objdir "%{OblikovatiRootDirectory}/Oblikovati/Resources/Scripts/Intermediates"
 
 	links {
 		"Coral.Managed"
 	}
 
-	--vsprops {
-	--	AppendTargetFrameworkToOutputPath = "false",
-	--	Nullable = "enable",
-	--	CopyLocalLockFileAssemblies = "true",
-	--	EnableDynamicLoading = "true",
-	--}
+	vsprops {
+		AppendTargetFrameworkToOutputPath = "false",
+		Nullable = "enable",
+		CopyLocalLockFileAssemblies = "true",
+		EnableDynamicLoading = "true",
+	}
 
 	files {
 		"%{OblikovatiRootDirectory}/lib_internal_source/Oblikovati-API/source/**.cs",
